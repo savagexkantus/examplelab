7c6fc92f79039a21fadbd7a172349ff3ba0d06f5
Dan Rusk <djrusk@gmail.com>
2015-08-23 22:23:40 -0400

    Commiting example lab

---
+++ src/main/java/edu/carrollcc/cis232/App.java
@@ -0,0 +1,13 @@
+package edu.carrollcc.cis232;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
